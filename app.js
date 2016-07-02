83fee0e0bc04e395fcb2125f5cc8712b574e6127
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2016-07-02 23:35:15 +1200

    chat client with sockets

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var app = express();
-var io = require('socket.io');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
- console.log('Example app listening on port 3000!');
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+  	socket.broadcast.emit('hi');
+  	 io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+// app.listen(3000, function () {
+//  console.log('Example app listening on port 3000!');
+// });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
